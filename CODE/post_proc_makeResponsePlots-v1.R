d182cd6a47757a90837083df8f410ee0b484516f
JoaoGoncalves <joaofgo@gmail.com>
2020-06-20 17:16:27 +0100

    Changes response plots

--- CODE/post_proc_makeResponsePlots-v1.R
+++ CODE/post_proc_makeResponsePlots-v1.R
@@ -6,43 +6,111 @@ library(dplyr)
 library(ggExtra)
 library(tidyr)
 library(biomod2plus)
-#source("D:/MyDocs/R-dev/biomod2plus/R/biomod2plus.R")
 
-setwd("./OUT")
+#setwd("./OUT")
 
-load("Allsprecords_ModObjects.RData")
+#source("D:/R-dev/biomod2plus/R/biomod2plus.R")
 
-
-DF <- values(current) %>% na.omit %>% as.data.frame
-colnames(DF)
-
-
-## ------------------------------------------------------------------------ ##
-## ------------------------------------------------------------------------ ##
-## ------------------------------------------------------------------------ ##
-
-# allModAlgos <- c("GLM", "GBM", "GAM", "CTA", "ANN", "FDA", "MARS", 
-#                  "RF", "MAXENT.Phillips", "MAXENT.Tsuruoka")
-# 
-allModAlgos <- c("GBM", "GAM", "RF")
-
-for(modAlgo in allModAlgos){
-  cat("Processing algorithm:",modAlgo,".....\n\n")
-  myMods <- BIOMOD_LoadModels(myBiomodModelOut, models=modAlgo)
+objs <- list.files(pattern=".RData$")
   
-  responsePlots(myBiomodModelOut, Data=DF, modelsToUse = myMods, 
-                showVars="all", fixedVarMetric = 'mean', plotStdErr = TRUE,
-                addMarginalPlot = TRUE, marginPlotType = "histogram",
-                filePrefix="ResponsePlot_", fileSuffix=paste("_",modAlgo,sep=""),
-                outFolder = "./Allsprecords/ResponsePlots", height=4, 
-                width=4, plot=FALSE, save=TRUE)
-  rm(list=myMods)
-  cat(" done.\n\n")
+#   c(
+# "Allsprecords_ModObjects.RData",
+# "AriasellaLusitanica_ModObjects.RData",
+# "AriasellaSp1_ModObjects.RData",
+# "Cluster1_ModObjects.RData",
+# "Cluster2_ModObjects.RData",
+# "Cluster4_ModObjects.RData"
+#           )
+
+outFolders <- c(
+"Allsprecords",
+"AriasellaLusitanica",
+"AriasellaSp1",
+"AriasellaSp2",
+"AriasellaSp3",
+"Cluster1",
+"Cluster2",
+"Cluster4")
+          
+
+      
+i <- 0
+for(obj in objs){
+  
+  i <- i+1
+  outFolderName <- outFolders[i]
+  #source("D:/MyDocs/R-dev/biomod2plus/R/biomod2plus.R")
+  
+  outFn <- paste("./_ResponsePlots/",outFolderName,"/",sep="")
+  
+  if(!dir.exists(outFn)){
+    dir.create(outFn)
+  }
+  
+  load(obj)
+  
+  DF <- values(current) %>% na.omit %>% as.data.frame
+  colnames(DF)
+  
+  modEvalData <- get_evaluations(myBiomodModelOut)
+  
+  evalDF.TSS <- as.data.frame(myBiomodModelEval["TSS","Testing.data",,,])
+  
+  # Top 3 modelling algorithms (by average TSS score) used 
+  # to make response curves
+  #
+  # bestModAlgos <- apply(evalDF.TSS,1,mean) %>% 
+  #   round(2) %>% 
+  #   sort(decreasing = TRUE) %>% 
+  #   names %>% 
+  #   `[`(1:3)
+  
+  bestModAlgos<-"GBM"
+  
+  ## ------------------------------------------------------------------------ ##
+  ##
+  # allModAlgos <- c("GLM", "GBM", "GAM", "CTA", "ANN", "FDA", "MARS", 
+  #                  "RF", "MAXENT.Phillips", "MAXENT.Tsuruoka")
+  # 
+  # allModAlgos <- c("GBM", "GAM", "RF")
+  
+  # sort(unique(unlist(lapply(strsplit(modelsToUse,"_"), 
+  # FUN = function(x) `[`(x,length(x))))))
+  
+  
+  for(modAlgo in bestModAlgos){
+    cat("Processing algorithm:",modAlgo,".....\n\n")
+    
+    modelsToUse <- BIOMOD_LoadModels(myBiomodModelOut, models=modAlgo)
+    
+    responsePlots(biomodModelOut = myBiomodModelOut, 
+                  Data           = DF, 
+                  modelsToUse    = modelsToUse, 
+                  showVars       = "all", 
+                  fixedVarMetric = 'mean', 
+                  plotStdErr     = TRUE,
+                  addMarginalPlot = TRUE, 
+                  marginPlotType  = "histogram",
+                  filePrefix      = "ResponsePlot_", 
+                  fileSuffix      = paste("_",modAlgo,sep=""),
+                  outFolder       = paste("./_ResponsePlots/",outFolderName,"/",sep=""), 
+                  height          = 4, 
+                  width           = 4, 
+                  plot            = FALSE, 
+                  save            = TRUE,
+                  saveCSV         = TRUE)
+    
+    rm(list=modelsToUse)
+    cat(" done.\n\n")
+  }
+
+  #rm(list=ls())
+
 }
 
 
 
-rm(list=ls()[grepl("Allsprecords_",ls())])
+#rm(list=ls()[grepl("Allsprecords_",ls())])
 
 
 ## ------------------------------------------------------------------------ ##
